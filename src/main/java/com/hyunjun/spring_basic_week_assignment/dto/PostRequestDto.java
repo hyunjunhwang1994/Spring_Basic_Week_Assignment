1b730d4b4825c2456f8ec8c684c05d27b8576e2d
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-26 18:56:23 +0900

    PostRequestDto

---
+++ src/main/java/com/hyunjun/spring_basic_week_assignment/dto/PostRequestDto.java
@@ -0,0 +1,13 @@
+package com.hyunjun.spring_basic_week_assignment.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+
+}
