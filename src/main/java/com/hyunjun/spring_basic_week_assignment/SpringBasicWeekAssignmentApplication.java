b8cd1d26e6d11746c68fed9dd135a7418fe38ba5
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-27 16:05:22 +0900

    etc

--- src/main/java/com/hyunjun/spring_basic_week_assignment/SpringBasicWeekAssignmentApplication.java
+++ src/main/java/com/hyunjun/spring_basic_week_assignment/SpringBasicWeekAssignmentApplication.java
@@ -2,7 +2,10 @@ package com.hyunjun.spring_basic_week_assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBasicWeekAssignmentApplication {
 
