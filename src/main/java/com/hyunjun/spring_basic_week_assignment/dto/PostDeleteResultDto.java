14cbc6dd5b745a706e998f848e709d7eb54ff253
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-27 16:04:11 +0900

    Update Dto

---
+++ src/main/java/com/hyunjun/spring_basic_week_assignment/dto/PostDeleteResultDto.java
@@ -0,0 +1,16 @@
+package com.hyunjun.spring_basic_week_assignment.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostDeleteResultDto {
+
+  boolean result;
+
+
+}
--- src/main/java/com/hyunjun/spring_basic_week_assignment/dto/PostRequestDto.java
+++ src/main/java/com/hyunjun/spring_basic_week_assignment/dto/PostRequestDto.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 public class PostRequestDto {
 
     private String title;
-    private String content;
+    private String contents;
     private String author;
+
+    private String password;
 
 
 }
