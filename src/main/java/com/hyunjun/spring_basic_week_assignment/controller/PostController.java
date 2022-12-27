facbfe22b2e798482afd8e34620a4d8135334deb
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-27 16:05:03 +0900

    Update Controller

---
+++ src/main/java/com/hyunjun/spring_basic_week_assignment/controller/PostController.java
@@ -0,0 +1,93 @@
+package com.hyunjun.spring_basic_week_assignment.controller;
+
+
+import com.hyunjun.spring_basic_week_assignment.dto.PostDeleteResultDto;
+import com.hyunjun.spring_basic_week_assignment.dto.PostRequestDto;
+import com.hyunjun.spring_basic_week_assignment.entity.Post;
+import com.hyunjun.spring_basic_week_assignment.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+
+    @GetMapping("/")
+    public ModelAndView home() {
+        return new ModelAndView("index");
+    }
+
+
+    @GetMapping("/upload/post")
+    public ModelAndView uploadPost() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("uploadpost");
+        return modelAndView;
+    }
+
+    // 글 입력
+    @PostMapping("/api/post")
+    public Post createPost(@RequestBody PostRequestDto requestDto) {
+
+
+        return postService.createPost(requestDto);
+    }
+
+    // 전체 글 조회
+    @GetMapping("/api/posts")
+    public List<Post> getPosts() {
+        return postService.getPosts();
+    }
+
+
+    // 선택 글 조회
+    @GetMapping("/api/post/{id}")
+    public ModelAndView readPost(@PathVariable Long id) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("post");
+        modelAndView.addObject(postService.readPost(id));
+
+        return modelAndView;
+
+    }
+
+    // 선택 글 삭제 (비밀번호 매칭)
+    @DeleteMapping("/api/post/{id}")
+    public PostDeleteResultDto deletePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
+
+
+        boolean result = postService.deletePost(id, requestDto.getPassword());
+
+        if(result == true){
+
+
+            return new PostDeleteResultDto(true);
+        }else{
+            return new PostDeleteResultDto(false);
+        }
+
+    }
+
+
+    // 선택 글 수정
+    @PutMapping("/api/post/{id}")
+    public Post updatePost(@PathVariable long id, @RequestBody PostRequestDto requestDto) {
+
+
+        return postService.updatePost(id, requestDto);
+
+
+    }
+
+}
